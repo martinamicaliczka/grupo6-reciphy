2f6f16af6087de907da7fad2706280b69a69504d
Ema Fresco <efresco@udesa.edu.ar>
2024-11-14 19:41:49 -0300

    hice el login y el java

---
+++ js/login.js
@@ -0,0 +1,15 @@
+let mailvacio = document.querySelector(".mailvacio");
+let email = document.querySelector("#email");
+let contrasennia = document.querySelector("#contrasennia");
+let form = document.querySelector(".form");
+let contravacio = document.querySelector(".contravacio");
+form.addEventListener("submit", function validar(event) {
+    if (email.value === "") {
+        mailvacio.innerText = "Por favor complete el campo";
+        event.preventDefault();
+    }
+    if (contrasennia.value === "") {
+        contravacio.innerText = "Por favor complete el campo";
+        event.preventDefault();
+    }
+});
