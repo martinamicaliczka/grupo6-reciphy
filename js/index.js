a702eb15c9be9f90971f4a39b8fd9cff2c7c495d
Martina Michaliczka <michamartu05@gmail.com>
2024-11-14 20:37:42 -0300

    solucioné errores del java del index

--- js/index.js
+++ js/index.js
@@ -1,18 +1,20 @@
-let search = document.querySelector("#Buscar"); 
+let buscar = document.querySelector("#buscar"); 
 let formulario = document.querySelector(".formulario");
 formulario.addEventListener("submit", function validar(event){
-    let valorSearch = search.value;
-    if (valorSearch === ""){
+    if (buscar.value === ""){
         alert("El texto esta vacio");
         event.preventDefault();
     }
-    else if (valorSearch.length < 3) {
+    else if (buscar.value.length < 3) {
         alert("El texto tiene que tener una longitud mayor a 3 caracteres");
-        event.preventDefault(); }
+        event.preventDefault();
+    }
    });
-const url = fetch('https://dummyjson.com/recipes')
+   
+let url = ('https://dummyjson.com/recipes?limit=10&skip=10&select=name,image');
 let recetas_seccion = document.querySelector(".recetas_seccion");
 let cargar_mas = document.querySelector("#cargar_mas");
+
 fetch(url)
 .then(function(data){
     return data.json();
@@ -20,7 +22,7 @@ fetch(url)
 .then(function(results) { 
     let recipe = ""; 
     let dato = results.recipes;
-    for(let i=0; dato.length; i++){
+    for(let i=0; i<dato.length; i++){
         recipe+= `
         <article class="recipe_card">
         <img src="${dato[i].image}" alt="${dato[i].name}">
@@ -31,7 +33,7 @@ fetch(url)
         </div>
         </article>`;
     }
-    recetas_seccion.innerHTML += recipe;
+    recetas_seccion.innerHTML = recipe;
 })
 .catch(function(error) {
     return console.log(error);
