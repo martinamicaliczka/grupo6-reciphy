e1e167307a6ad7175594374a0df2d8ee9ca95ce5
Martina Michaliczka <michamartu05@gmail.com>
2024-11-15 11:33:14 -0300

    hicimos con ema el cargar mas y arreglue unos detales del login y el index

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
-let buscar = document.querySelector("#buscar"); 
-let formulario = document.querySelector(".formulario");
-formulario.addEventListener("submit", function validar(event){
-    if (buscar.value === ""){
+//validamos elformulario
+let buscar = document.querySelector("#buscar");
+let formularioIndex = document.querySelector(".formularioIndex");
+formularioIndex.addEventListener("submit", function validar(event) {
+    if (buscar.value === "") {
         alert("El texto esta vacio");
         event.preventDefault();
     }
@@ -9,21 +10,25 @@ formulario.addEventListener("submit", function validar(event){
         alert("El texto tiene que tener una longitud mayor a 3 caracteres");
         event.preventDefault();
     }
-   });
-   
-let url = ('https://dummyjson.com/recipes?limit=10&skip=10&select=name,image');
+});
+
+//API
+let limit = 10
+let url = (`https://dummyjson.com/recipes?limit=${limit}&skip=0&select=name,image`);
 let recetas_seccion = document.querySelector(".recetas_seccion");
 let cargar_mas = document.querySelector("#cargar_mas");
 
-fetch(url)
-.then(function(data){
-    return data.json();
-})
-.then(function(results) { 
-    let recipe = ""; 
-    let dato = results.recipes;
-    for(let i=0; i<dato.length; i++){
-        recipe+= `
+
+function buscarFetch(url) {
+    fetch(url)
+        .then(function (data) {
+            return data.json();
+        })
+        .then(function (results) {
+            let recipe = "";
+            let dato = results.recipes;
+            for (let i = 0; i < dato.length; i++) {
+                recipe += `
         <article class="recipe_card">
         <img src="${dato[i].image}" alt="${dato[i].name}">
         <div class="recetas_info">
@@ -32,9 +37,18 @@ fetch(url)
         <a href="./resultados.html?idReceta=${dato[i].id}">Ver detalle</a>
         </div>
         </article>`;
+            }
+            recetas_seccion.innerHTML = recipe;
+        })
+        .catch(function (error) {
+            return console.log(error);
+        });
+}
+buscarFetch(`https://dummyjson.com/recipes?limit=${limit}&skip=0&select=name,image`)
+cargar_mas.addEventListener("click", function (e) {
+    limit += 10
+    buscarFetch(`https://dummyjson.com/recipes?limit=${limit}&skip=0&select=name,image`);
+    if (limit == 50) {
+        cargar_mas.style.display = "none"
     }
-    recetas_seccion.innerHTML = recipe;
 })
-.catch(function(error) {
-    return console.log(error);
-});
